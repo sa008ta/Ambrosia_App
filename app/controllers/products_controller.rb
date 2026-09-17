732a09518108f8db4010e9070f9a53507f38d6bf
sa008ta <saki008ta@gmail.com>
2026-09-17 14:11:18 +0900

    写真ファイル追加可能

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -45,6 +45,6 @@ class ProductsController < ApplicationController
   end
 
   def product_params
-    params.require(:product).permit(:name, :description, :price, :category, :image_url, :stock_quantity)
+    params.require(:product).permit(:name, :description, :price, :category, :image, :stock_quantity)
   end
 end
